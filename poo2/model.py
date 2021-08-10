872459d15e1c0c856c39dbd324d47b6cf76ba7fa
Daniel Matos <danielzsmattos@gmail.com>
2021-08-10 01:53:17 -0300

    Herança múltipla, Resolução da ordem de chamada de métodos (MRO) & Mixins

--- poo2/model.py
+++ poo2/model.py
@@ -18,7 +18,9 @@ class Programa:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
-
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - Likes: {self.likes}'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -35,21 +37,41 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-
 def __str__(self):
     return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas        #Chamando o construtor da list e passando os programas
+        #super().__init__(programas)    
+    
+    def __getitem__(self, item)-> None:
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+    
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('Todo mundo em pânico', 1999, 1000)
+demolidor = Serie('Demolidor', 2015, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-listinha = [atlanta, vingadores]
+minha_lista = [atlanta, vingadores, tmep, demolidor]
+playlist_fim_de_semana = Playlist('Play fim de semana', minha_lista)
 
-for programa in listinha:
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
